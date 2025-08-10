473d91c7b5d8d0f7390c56957dcb16c8d3004e11
Anatoly <yatskovanatoly@gmail.com>
2025-08-10 17:47:36 +0300

    fix: minor

--- components/Waves.tsx
+++ components/Waves.tsx
@@ -32,55 +32,51 @@ const WaveAnimation: FC<PropsWithChildren & { playing: PlayerState }> = ({
 	)
 }
 
-const WavesLeft: FC<{ src: string[] }> = ({ src }) => {
-	return (
-		<div className='absolute right-full h-1/2 w-1/2 top-2/5 flex gap-2 sm:gap-6 max-sm:hidden'>
-			<div
-				className={`animate-fadeInOut relative opacity-0 h-full w-1/4`}
-				style={{ animationDelay: '500ms' }}
-			>
-				<Image src={src[0]} alt='wave-l-1' fill sizes='100vw' />
-			</div>
-			<div
-				className={`animate-fadeInOut relative opacity-0 top-1/5 h-2/3 w-1/6`}
-				style={{ animationDelay: '250ms' }}
-			>
-				<Image src={src[1]} alt='wave-l-2' fill sizes='100vw' />
-			</div>
-			<div
-				className={`animate-fadeInOut relative opacity-0 top-2/5 h-2/5 w-1/8`}
-				style={{ animationDelay: '0ms' }}
-			>
-				<Image src={src[2]} alt='wave-l-3' fill sizes='100vw' />
-			</div>
+const WavesLeft: FC<{ src: string[] }> = ({ src }) => (
+	<div className='absolute right-full h-1/2 w-1/2 top-2/5 flex gap-2 sm:gap-6 max-sm:hidden'>
+		<div
+			className={`animate-fadeInOut relative opacity-0 h-full w-1/4`}
+			style={{ animationDelay: '500ms' }}
+		>
+			<Image src={src[0]} alt='wave-l-1' fill sizes='100vw' />
 		</div>
-	)
-}
+		<div
+			className={`animate-fadeInOut relative opacity-0 top-1/5 h-2/3 w-1/6`}
+			style={{ animationDelay: '250ms' }}
+		>
+			<Image src={src[1]} alt='wave-l-2' fill sizes='100vw' />
+		</div>
+		<div
+			className={`animate-fadeInOut relative opacity-0 top-2/5 h-2/5 w-1/8`}
+			style={{ animationDelay: '0ms' }}
+		>
+			<Image src={src[2]} alt='wave-l-3' fill sizes='100vw' />
+		</div>
+	</div>
+)
 
-const WavesRight: FC<{ src: string[] }> = ({ src }) => {
-	return (
-		<div className='absolute -right-3/5 h-1/2 w-1/2 top-2/5 flex gap-2 sm:gap-6 max-sm:hidden'>
-			<div
-				className={`animate-fadeInOut relative opacity-0 top-2/5 h-2/5 w-1/8`}
-				style={{ animationDelay: '0ms' }}
-			>
-				<Image src={src[0]} alt='wave-r-1' fill />
-			</div>
-			<div
-				className={`animate-fadeInOut relative opacity-0 top-1/5 h-2/3 w-1/6`}
-				style={{ animationDelay: '250ms' }}
-			>
-				<Image src={src[1]} alt='wave-r-2' fill />
-			</div>
-			<div
-				className={`animate-fadeInOut relative opacity-0 h-full w-1/4`}
-				style={{ animationDelay: '500ms' }}
-			>
-				<Image src={src[2]} alt='wave-r-3' fill />
-			</div>
+const WavesRight: FC<{ src: string[] }> = ({ src }) => (
+	<div className='absolute -right-3/5 h-1/2 w-1/2 top-2/5 flex gap-2 sm:gap-6 max-sm:hidden'>
+		<div
+			className={`animate-fadeInOut relative opacity-0 top-2/5 h-2/5 w-1/8`}
+			style={{ animationDelay: '0ms' }}
+		>
+			<Image src={src[0]} alt='wave-r-1' fill />
 		</div>
-	)
-}
+		<div
+			className={`animate-fadeInOut relative opacity-0 top-1/5 h-2/3 w-1/6`}
+			style={{ animationDelay: '250ms' }}
+		>
+			<Image src={src[1]} alt='wave-r-2' fill />
+		</div>
+		<div
+			className={`animate-fadeInOut relative opacity-0 h-full w-1/4`}
+			style={{ animationDelay: '500ms' }}
+		>
+			<Image src={src[2]} alt='wave-r-3' fill />
+		</div>
+	</div>
+)
 
 const waves = [
 	{
